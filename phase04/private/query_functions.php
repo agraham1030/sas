64cbd7cf81e18660f8041af82944c7b6f288e2f3
Alec Graham <agraham1030@gmail.com>
2023-04-17 14:53:55 -0400

    adds insert salamander capabilities

--- phase04/private/query_functions.php
+++ phase04/private/query_functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function find_all_salamanders() {
+function find_all_salamanders()
+{
     global $db;
     $sql = "SELECT * FROM salamander ";
     $sql .= "ORDER BY name ASC";
@@ -9,14 +10,36 @@ function find_all_salamanders() {
     return $result;
 }
 
-function find_salamander_by_id ($id) {
+function find_salamander_by_id($id)
+{
     global $db;
     $sql = "SELECT * FROM salamander ";
     $sql .= "WHERE id='" . $id . "'";
-    
+
     $result = mysqli_query($db, $sql);
     confirm_result_set($result, $sql);
     $salamander = mysqli_fetch_assoc($result);
     mysqli_free_result($result);
     return $salamander;
+}
+
+function insert_salamander($name, $habitat, $description)
+{
+    global $db;
+    $sql = "INSERT INTO salamander ";
+    $sql .= "(name, habitat, description) ";
+    $sql .= "VALUES (";
+    $sql .= "'" . $name . "',";
+    $sql .= "'" . $habitat . "',";
+    $sql .= "'" . $description . "'";
+    $sql .= ")";
+    $result = mysqli_query($db, $sql);
+
+    if ($result) {
+        return true;
+    } else {
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit;
+    }
 }
--- phase04/public/salamanders/new.php
+++ phase04/public/salamanders/new.php
@@ -1,5 +1,18 @@
 <?php require_once('../../private/initialize.php'); 
-include(SHARED_PATH . '/salamander-header.php');
-echo "<h1>Stub for New Salamander</h1>";
 
-include(SHARED_PATH . '/salamander-footer.php'); ?>
+$pageTitle = 'Create Salamander';
+include(SHARED_PATH . '/salamander-header.php');?>
+
+<a href="<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>
+<h1>Create Salamander</h1>
+<form action="<?php echo url_for('salamanders/create.php'); ?>" method="post">
+  <label for="name">Salamander Name: </label><br>
+  <input type="text" name="name"><br>
+  <label for="habitat">Habitat: </label><br>
+  <textarea rows="6" cols="50" name="habitat"></textarea><br>
+  <label for="description">Description: </label><br>
+  <textarea rows="6" cols="50" name ="description"></textarea><br>
+  <input type="submit" value="Create Salamander">
+</form>
+
+<?php include(SHARED_PATH . '/salamander-footer.php'); ?>
