7f7850be83f118094a9bb6b4c3775a6e3ced85fe
Alec Graham <agraham1030@gmail.com>
2023-04-17 14:01:39 -0400

    adds find and display a single record

--- phase04/private/query_functions.php
+++ phase04/private/query_functions.php
@@ -8,3 +8,15 @@ function find_all_salamanders() {
     confirm_result_set($result, $sql);
     return $result;
 }
+
+function find_salamander_by_id ($id) {
+    global $db;
+    $sql = "SELECT * FROM salamander ";
+    $sql .= "WHERE id='" . $id . "'";
+    
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result, $sql);
+    $salamander = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    return $salamander;
+}
--- phase04/public/salamanders/show.php
+++ phase04/public/salamanders/show.php
@@ -1,14 +1,19 @@
 <?php require_once('../../private/initialize.php');
 
 $id = $_GET['id'] ?? '1'; // PHP > 7.0
+
+$salamander = find_salamander_by_id($id);
+
 $page_title = 'View Salamander';
 include(SHARED_PATH . '/salamander-header.php'); 
 
 ?>
-
-
   <a href="<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>
 
-   <p> Page ID: <?= h($id); ?> </p>
-
+  <h1>Salamander Details</h1>
+  <p><strong>ID:</strong> <?= h($salamander['id']); ?> </p>
+  <p><strong>Name:</strong> <?php echo h($salamander['name']) ?></p>
+  <p><strong>Habitat:</strong> <?php echo h($salamander['habitat']) ?></p>
+  <p><strong>Description:</strong> <?php echo h($salamander['description']) ?></p>
+   
 <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
